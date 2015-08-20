0371e1fcfdd2fc4e93598356b490841d18c1b37c
Clayton Parker <me@claytron.com>
2015-08-20 19:10:03 -0400

    explain the options better in interactive help

--- lib/augury/cli.rb
+++ lib/augury/cli.rb
@@ -4,8 +4,12 @@ require 'augury'
 module Augury
   class CLI < Thor
     desc 'generate USERNAME [PATH]', 'Generate a fortune file for the given username'
-    option :width, :type => :numeric, :aliases => :w
-    option :append, :type => :boolean, :aliases => :a
+    option :width,
+      :type => :numeric, :aliases => '-w', :default => 72,
+      :desc => 'The maximum number of columns that will be written on a line.'
+    option :append,
+      :type => :boolean, :aliases => '-a',
+      :desc => 'If set, the target path will be appended to instead of overwritten'
     def generate(username, *path)
       path = File.expand_path(path[0] || username)
       begin
